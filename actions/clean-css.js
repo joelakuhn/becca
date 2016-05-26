ebcc2ed534636146da51ca8e296fda2a51eb7087
Joel Kuhn <joelakuhn@gmail.com>
2016-05-26 16:51:54 -0400

    Added options

--- actions/clean-css.js
+++ actions/clean-css.js
@@ -3,8 +3,8 @@ var CleanCSS = require('clean-css');
 module.exports = {
   name: 'clean_css',
   sync: true,
-  run: function(state) {
+  run: function(state, options) {
     state.file.setExtension('.min.css');
-    state.contents = new CleanCSS().minify(state.contents).styles;
+    state.contents = new CleanCSS(options).minify(state.contents).styles;
   }
 }
