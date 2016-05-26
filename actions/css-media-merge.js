6906a4795815da821a005f42bdd406e4613ce2bc
Joel Kuhn <joelakuhn@gmail.com>
2016-05-26 16:52:20 -0400

    Implemented css_media_merge

--- actions/css-media-merge.js
+++ actions/css-media-merge.js
@@ -1,6 +1,9 @@
+var cmm = require('css-media-merge');
+
 module.exports = {
   name: 'css_media_merge',
   sync: true,
   run: function(state, options) {
+  	state.contents = cmm.merge(state.contents, options);
   }
 }
