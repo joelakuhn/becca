6054531e655eec59f705f467277f95ead7de96b9
Joel Kuhn <joelakuhn@gmail.com>
2016-05-31 13:45:41 -0400

    Moved requires into 'run' to minimize module loading.

--- actions/clean-css.js
+++ actions/clean-css.js
@@ -1,9 +1,9 @@
-var CleanCSS = require('clean-css');
-
 module.exports = {
   name: 'clean_css',
   sync: true,
   run: function(state, options) {
+    var CleanCSS = require('clean-css');
+
     state.file.setExtension('.min.css');
     state.contents = new CleanCSS(options).minify(state.contents).styles;
   }
--- actions/css-media-merge.js
+++ actions/css-media-merge.js
@@ -1,9 +1,8 @@
-var cmm = require('css-media-merge');
-
 module.exports = {
   name: 'css_media_merge',
   sync: true,
   run: function(state, options) {
+    var cmm = require('css-media-merge');
   	state.contents = cmm.merge(state.contents, options);
   }
 }
