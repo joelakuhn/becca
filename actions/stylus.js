886ad4cdade9d41291cfc17a19012d05731a3383
Joel Kuhn <joelakuhn@gmail.com>
2016-06-01 14:57:15 -0400

    Changed stylus action to sync

--- actions/stylus.js
+++ actions/stylus.js
@@ -2,9 +2,9 @@ var stylus   = require('stylus');
 
 module.exports = {
   name: 'stylus',
+  sync: true,
   run: function(state, callback) {
     state.contents = stylus.render(state.contents);
-    callback(null, state);
   },
   path: function(state, args) {
     state.file.setExtension('.css');
