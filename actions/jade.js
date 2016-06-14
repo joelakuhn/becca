b445092dd0d72efe6f05cb9bf177fd475911eff9
Joel Kuhn <joelakuhn@gmail.com>
2016-06-14 14:51:53 -0400

    Added jade action

---
+++ actions/jade.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: 'jade',
+	sync: true,
+	run: function(state, args) {
+		var jade = require('jade');
+		state.contents = jade.render(state.contents);
+	},
+	path: function(state, args) {
+		state.file.setExtension('.html');
+	}
+}
