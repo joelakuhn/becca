d75f5f8d790b7d6c2175b2aa60b8c7e5355b6c98
Joel Kuhn <joelakuhn@gmail.com>
2016-06-29 12:29:54 -0400

    Added the current file path to the 'paths' option for the stylus action. This allows require statements to work relative to the file location as they should.

--- actions/stylus.js
+++ actions/stylus.js
@@ -3,8 +3,10 @@ var stylus   = require('stylus');
 module.exports = {
   name: 'stylus',
   sync: true,
-  run: function(state, callback) {
-    state.contents = stylus.render(state.contents);
+  run: function(state, callback, options) {
+    if (!options) options = {};
+    options.paths = [ state.file.dir() ];
+    state.contents = stylus.render(state.contents, options);
   },
   path: function(state, args) {
     state.file.setExtension('.css');
