31f1b02ed8c3fbad6a7e42a703346c64c3a25f28
Joel Kuhn <joelakuhn@gmail.com>
2016-11-05 11:53:58 -0400

    Switched stylus action to asynchronous and moved the require so it doesn't break if stylus isn't installed and you're not using it.

--- actions/stylus.js
+++ actions/stylus.js
@@ -1,12 +1,22 @@
-var stylus   = require('stylus');
-
 module.exports = {
   name: 'stylus',
-  sync: true,
   run: function(state, callback, options) {
+    var stylus   = require('stylus');
+
     if (!options) options = {};
+
     options.paths = [ state.file.dir() ];
-    state.contents = stylus.render(state.contents, options);
+
+    stylus.render(state.contents, options, function(err, css) {
+      if (err) {
+        callback(err);
+      }
+      else {
+        state.contents = css;
+        callback(state);
+      }
+    });
+
   },
   path: function(state, args) {
     state.file.setExtension('.css');
