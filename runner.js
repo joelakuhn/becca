efb2fd2efd4003ffacffe0dcaec3ffa621ca6954
Joel Kuhn <joelakuhn@gmail.com>
2016-11-11 23:34:58 -0500

    Added is_running tracking which allows watched pipelines to start nodes that haven' already been run if a coalesce action depends on them

--- runner.js
+++ runner.js
@@ -17,9 +17,7 @@ Runner.prototype.start = function() {
 
 function runPipeline(pipeline) {
   pipeline.nodes.forEach(function(node) {
-    Runner.runNode(node, {
-      file: new FilePath(node.file)
-    });
+    Runner.runNode(node);
   });
 }
 
@@ -97,15 +95,59 @@ function skip(node) {
 }
 
 function run_next(node, state) {
+  node.is_running = false;
   if (node.next) {
     Runner.runNode(node.next, state || node.state);
   }
 }
 
+function is_pipeline_running(node) {
+  var nNode = node;
+  var pNode = node;
+
+  if (node.is_running) return true;
+
+  while (nNode.next) {
+    if (nNode.is_running) return true;
+    nNode = nNode.next;
+  }
+
+  while (pNode.prev) {
+    if (pNode.is_running) return true;
+    pNode = pNode.prev;
+  }
+
+  return false;
+}
+
+function run_missing_prereqs(node) {
+  if (node.prev) {
+    node.prev.forEach(run_missing_prereqs);
+  }
+  else {
+    if (!is_pipeline_running(node)) {
+      Runner.runNode(node);
+    }
+  }
+}
+
 Runner.runNode = function(node, state) {
+  if (node.is_running) return;
+
+  if (arguments.length === 1) {
+    state = {
+      file: new FilePath(node.file)
+    }
+  }
+
   var args = node.args.slice();
 
-  if (!prereqs_met(node, state)) return;
+  if (!prereqs_met(node, state)) {
+    run_missing_prereqs(node);
+    return;
+  }
+
+  node.is_running = true;
 
   get_state(node, state, function(err, state) {
 
