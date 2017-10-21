0ae57263dae9c5615d27c611ac8f6c2daa05f811
Joel Kuhn <joelakuhn@gmail.com>
2017-10-21 01:21:10 -0400

    Added friendlier message when an action does not exist.

--- load-config.js
+++ load-config.js
@@ -3,7 +3,20 @@ var path = require('path');
 
 function load_config() {
   if (fs.existsSync('beccaconfig.js')) {
-    require(path.join(process.cwd(), 'beccaconfig.js'));
+    try {
+      require(path.join(process.cwd(), 'beccaconfig.js'));
+    }
+    catch (e) {
+      var error_match = e.message.match(/becca.+\.(.+) is not a function/);
+      if (error_match) {
+        console.log('"' + error_match[1] + '"' + ' is not a becca action.');
+        process.exit();
+      }
+      else {
+        throw e;
+      }
+    }
+
     return true;
   }
   else {
